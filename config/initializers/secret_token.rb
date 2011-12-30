2d6fac907ba88726bcbb1ee74ac3c2a3b61e765b
Giorgio G <george@concretemart.com.au>
2011-12-31 08:58:28 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailInstApp::Application.config.secret_token = '544ede6369ad6a954f3ee40015b966cef91163f3e86c08cd2d60894cdd29dfc4b5816c82634b91ce80519179506eea7af4930cce95a9b1214013c93f30571eab'
